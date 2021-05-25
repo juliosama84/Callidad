e85bcfef18f7b40c5cf8eef1c62027412e80c106
julio123-0 <julyiocesar84@gmail.com>
2021-05-24 21:58:25 -0500

    mas pruebas

--- domainMock.py
+++ domainMock.py
@@ -1,50 +1,50 @@
-from unittest import mock
-import unittest
-from unittest.mock import patch,MagicMock
-from domainInsercion import insertarDatos
-from pokemon import pokebase
-from entrenador import entrenadorbase
+# from unittest import mock
+# import unittest
+# from unittest.mock import patch,MagicMock
+# from domainInsercion import insertarDatos
+# from pokemon import pokebase
+# from entrenador import entrenadorbase
 
 
-class testMockDomain(unittest.TestCase):
+# class testMockDomain(unittest.TestCase):
     
     
     
-    @patch('domainInsercion.insertarDatos.insert_entrenador_data',return_value=True)
-    def test_funcionaCorrecto(self,mock_get):
-     llamar=insertarDatos()
-     resultado_esperado=True
-     resultado_actual=llamar.insert_entrenador_data("mock")
+#     @patch('domainInsercion.insertarDatos.insert_entrenador_data',return_value=True)
+#     def test_funcionaCorrecto(self,mock_get):
+#      llamar=insertarDatos()
+#      resultado_esperado=True
+#      resultado_actual=llamar.insert_entrenador_data("mock")
      
-     assert resultado_esperado==resultado_actual
+#      assert resultado_esperado==resultado_actual
         
-    @patch('domainInsercion.insertarDatos.eliminarEquipo',return_value="Se borraron los pokemon correctamente")
-    def test_eliminar_equipo(self,mock_get):
-     llamar=insertarDatos()
+#     @patch('domainInsercion.insertarDatos.eliminarEquipo',return_value="Se borraron los pokemon correctamente")
+#     def test_eliminar_equipo(self,mock_get):
+#      llamar=insertarDatos()
     
-     resultado_esperado="Se borraron los pokemon correctamente"
-     resultado_actual=llamar.eliminarEquipo("mock",1)
+#      resultado_esperado="Se borraron los pokemon correctamente"
+#      resultado_actual=llamar.eliminarEquipo("mock",1)
      
-     assert resultado_esperado==resultado_actual
+#      assert resultado_esperado==resultado_actual
      
-    @patch('domainInsercion.insertarDatos.eliminarEquipo',return_value="No se eliminó")
-    def test_eliminar_equipo_dos(self,mock_get):
-     llamar=insertarDatos()
+#     @patch('domainInsercion.insertarDatos.eliminarEquipo',return_value="No se eliminó")
+#     def test_eliminar_equipo_dos(self,mock_get):
+#      llamar=insertarDatos()
     
-     resultado_esperado="No se eliminó"
-     resultado_actual=llamar.eliminarEquipo("mock",1)
+#      resultado_esperado="No se eliminó"
+#      resultado_actual=llamar.eliminarEquipo("mock",1)
      
-     assert resultado_esperado==resultado_actual
+#      assert resultado_esperado==resultado_actual
     
-    @patch('domainInsercion.insertarDatos.get_entrenador_data',return_value=1)
-    def test_get_data_entrenador(self,mock_get):
-     llamar=insertarDatos()
+#     @patch('domainInsercion.insertarDatos.get_entrenador_data',return_value=1)
+#     def test_get_data_entrenador(self,mock_get):
+#      llamar=insertarDatos()
     
-     resultado_esperado=1
-     resultado_actual=llamar.get_entrenador_data("mock")
+#      resultado_esperado=1
+#      resultado_actual=llamar.get_entrenador_data("mock")
      
-     assert resultado_esperado==resultado_actual
+#      assert resultado_esperado==resultado_actual
 
 
-if __name__ == "__main__":
-    unittest.main()
+# if __name__ == "__main__":
+#     unittest.main()
